b55e6697060c83ceb1819debfb35930683d3326d
Sindhu <sindhujav18@gmail.com>
2020-03-02 11:19:50 -0600

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function multiply()
+function compare(string1,string2)
 {
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
-        document.getElementById("result").innerHTML = num1 * num2;
+        var a = document.getElementById("str1").value;
+        var b = document.getElementById("str2").value;
+        document.getElementById("result").innerHTML = Math.max(a.length,b.length);
 }
